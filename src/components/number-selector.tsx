c621a3a41ee92e5761d677f05e7303c8deea6768
Selyss <99344963+Selyss@users.noreply.github.com>
2024-09-22 14:41:47 +0000

    better num selector

--- src/components/number-selector.tsx
+++ src/components/number-selector.tsx
@@ -6,14 +6,15 @@ interface NumberSelectorProps {
 }
 
 export function NumberSelector({ selectedNumber, setSelectedNumber }: NumberSelectorProps) {
+    const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     return (
         <div className="flex justify-center space-x-2">
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => (
+            {numbers.map((num) => (
                 <Button
                     key={num}
                     onClick={() => setSelectedNumber(num)}
-                    variant={selectedNumber === num ? "default" : "outline"}
-                    size="icon"
+                    variant={"ghost"}
+                    className={`w-10 h-10 p-0 text-xl font-bold ${selectedNumber === num ? 'bg-purple-600 text-white' : 'bg-neutral-800 text-purple-400'} rounded-lg`}
                 >
                     {num}
                 </Button>
