e5ae64b206f440b72df6c46f66818bdfb72c2a23
Selyss <99344963+Selyss@users.noreply.github.com>
2024-09-24 21:16:59 +0000

    temp puzzle gen

--- src/server/api/routers/sudoku.ts
+++ src/server/api/routers/sudoku.ts
@@ -1,15 +1,32 @@
+import { eq } from "drizzle-orm";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { puzzles } from "~/server/db/schema";
+import { generatePuzzle } from "~/server/utils/sudokuGenerator";
 
 export const sudokuRouter = createTRPCRouter({
+  generatePuzzle: publicProcedure.mutation(async ({ ctx }) => {
+    const { puzzle, solution } = generatePuzzle();
+
+    const puzzleId = await ctx.db.insert(puzzles).values({
+      puzzle: JSON.stringify(puzzle),
+      solution: JSON.stringify(solution),
+    });
+    return {
+      id: puzzleId,
+      puzzle,
+      solution,
+    };
+    }),
+  });
+  
   getPuzzle: publicProcedure
     .input(z.object({ id: z.number() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
+    .query(async({ ctx, input }) => {
+      return await ctx.db.query.puzzles.findFirst({
+        where: eq(puzzles.id, input.id),
+      });
     }),
 
   create: publicProcedure
---
+++ src/server/utils/sudokuGenerator.ts
@@ -0,0 +1,19 @@
+// TODO: make my own api later
+
+import sudoku from 'sudoku';
+
+export function generatePuzzle(): { puzzle: number[][], solution: number[][] } {
+    const rawPuzzle = sudoku.makepuzzle();
+    const rawSolution = sudoku.solvepuzzle(rawPuzzle);
+
+    const puzzle = [];
+    const solution = [];
+
+
+    for (let i = 0; i < 9; i++) {
+        puzzle.push(rawPuzzle.slice(i * 9, (i + 1) * 9).map((cell: number | null) => cell !== null ? cell + 1 : 0));
+        solution.push(rawSolution.slice(i * 9, (i + 1) * 9).map((cell: number) => cell + 1));
+    }
+
+    return { puzzle, solution };
+}
